3d8b99166313099193515bb44fe5a273a99fa3d3
jeatzr <profesor.jeatienzar@iesazarquiel.es>
2024-06-14 12:50:15 +0200

    catch fetch errors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,13 @@ const newJokeButton = <HTMLButtonElement>document.getElementById("new-joke")
 const setupContainer = <HTMLParagraphElement>document.getElementById("setup-container")
 const punchButton = <HTMLButtonElement>document.getElementById("punch-button")
 const punchContainer = <HTMLDivElement>document.querySelector(".punch-container")
+const resultContainer = <HTMLDivElement>document.getElementById("result");
+const errorContainer = <HTMLDivElement>document.getElementById("error");
+
+resultContainer.style.display = "none"
+errorContainer.style.display = "none"
+
+let joke: Joke
 
 interface Joke {
   type: string,
@@ -13,26 +20,33 @@ interface Joke {
 }
 
 function showPunchLine(punchLine: string = "") {
-
-  let num = Math.round(Math.random() * 7)
+  let num = Math.round(Math.random() * 6)
   punchContainer.innerHTML = ""
-
   punchContainer.innerHTML += `<p>${punchLine}</p>`
   punchContainer.innerHTML += `<img src="img/lol${num}.gif" alt="LOL GIF"/>`
-
-
 }
 
-let joke: Joke
 
 newJokeButton.addEventListener("click", async () => {
 
-  const response = await fetch("https://official-joke-api.appspot.com/random_joke")
-  joke = await response.json()
-  console.log(joke)
+  try {
+    const response = await fetch("https://official-joke-api.appspot.com/random_joke")
+    joke = await response.json()
+    console.log(joke)
 
-  setupContainer.textContent = joke.setup
-  punchContainer.innerHTML = ""
+    errorContainer.style.display = "none"
+    resultContainer.style.display = "block"
+    setupContainer.textContent = joke.setup
+    punchContainer.innerHTML = ""
+
+  } catch (error) {
+    resultContainer.style.display = "none"
+    errorContainer.style.display = "block"
+    errorContainer.innerHTML = ""
+    errorContainer.innerHTML += `<p>Something went wrong with the connnection, it's no joke :(</p>`
+    errorContainer.innerHTML += `<img src = "img/sad-pikachu.gif" alt="no joke :("/>`
+  }
+
 
 })
 
